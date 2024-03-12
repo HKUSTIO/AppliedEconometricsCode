de737830a5baa7c9f630fac5af6b30ed4ece0af7
Kohei Kawaguchi <kohei.kawagucci@gmail.com>
2024-03-12 14:40:24 +0800

    added multiple testing files

--- R/function_inference.R
+++ R/function_inference.R
@@ -46,3 +46,285 @@ return_dgp_cluster <-
       )
     )
 }
+
+#'@export
+compute_t_statistics_null_effect <-
+  function(
+      N,
+      M,
+      alpha,
+      seed
+  ) {
+    set.seed(seed)
+
+    z <-
+      (
+        runif(N) >= 0.5
+      )
+
+    df_list <-
+      seq_len(M) %>%
+      purrr::map(
+        ~ tibble::tibble(
+          z = z,
+          y = rnorm(N)
+        )
+      )
+
+    result_list <-
+      df_list %>%
+      purrr::map(
+        ~ lm(
+          formula = y ~ z,
+          data = .
+        )
+      )
+
+    t_list <-
+      result_list %>%
+      purrr::map(
+        ~ summary(.) %>%
+          coef() %>%
+          .[
+            "zTRUE",
+            "t value"
+          ]
+      ) %>%
+      purrr::reduce(c)
+
+    return(t_list)
+  }
+
+#'@export
+compute_t_statistics_alternative_effect <-
+  function(
+    N,
+    M,
+    alpha,
+    seed
+  ) {
+    set.seed(seed)
+
+    z <-
+      (
+        runif(N) >= 0.5
+      )
+
+    df_list <-
+      seq_len(M-1) %>%
+      purrr::map(
+        ~ tibble::tibble(
+          z = z,
+          y = rnorm(N) + 0.3 * z
+        )
+      )
+
+    df_list <- c(
+      df_list,
+      seq(M, M) %>%
+        purrr::map(
+          ~ tibble::tibble(
+            z = z,
+            y = rnorm(N) + 0.5 * z
+          )
+        )
+    )
+
+    result_list <-
+      df_list %>%
+      purrr::map(
+        ~ lm(
+          formula = y ~ z,
+          data = .
+        )
+      )
+
+    t_list <-
+      result_list %>%
+      purrr::map(
+        ~ summary(.) %>%
+          coef() %>%
+          .[
+            "zTRUE",
+            "t value"
+          ]
+      ) %>%
+      purrr::reduce(c)
+
+    return(t_list)
+  }
+
+#'@export
+compute_p_value_mixed_effect <-
+  function(
+    N,
+    M,
+    M_0,
+    tau_population,
+    seed
+  ) {
+    z <-
+      (
+        runif(N) >= 0.5
+      )
+
+    df_list_1 <-
+      seq_len(M - M_0) %>%
+      purrr::map(
+        ~ tibble::tibble(
+            z = z,
+            y = rnorm(N) + tau_population * z
+          )
+      )
+
+    df_list_0 <-
+      seq_len(M_0) %>%
+      purrr::map(
+        ~ tibble::tibble(
+            z = z,
+            y = rnorm(N)
+          )
+      )
+
+    result_list_1 <-
+      df_list_1 %>%
+      purrr::map(
+        ~ lm(
+            formula = y ~ z,
+            data = .
+          )
+      )
+
+    result_list_0 <-
+      df_list_0 %>%
+      purrr::map(
+        ~ lm(
+            formula = y ~ z,
+            data = .
+          )
+      )
+
+      p_list_1 <-
+      result_list_1 %>%
+      purrr::map(
+        ~ summary(.) %>%
+          coef() %>%
+          .[
+            "zTRUE",
+            "Pr(>|t|)"
+          ]
+      ) %>%
+      purrr::reduce(c)
+
+    p_list_0 <-
+      result_list_0 %>%
+      purrr::map(
+        ~ summary(.) %>%
+          coef() %>%
+          .[
+            "zTRUE",
+            "Pr(>|t|)"
+          ]
+      ) %>%
+      purrr::reduce(c)
+
+    return(
+      list(
+        p_list_1 = p_list_1,
+        p_list_0 = p_list_0
+      )
+    )
+  }
+
+#'@export
+compute_p_value_benjamini_hotchberg <-
+  function(
+    p_list_1,
+    p_list_0,
+    alpha
+  ) {
+    p_list_sorted <-
+      c(
+        p_list_1,
+        p_list_0
+      ) %>%
+      sort()
+    M <-
+      length(p_list_sorted)
+    i <- M
+    flag_continue <- TRUE
+    while (
+      flag_continue & i > 1
+    ) {
+      test <- p_list_sorted[i] <= (i / M) * alpha
+      if (
+        test
+      ) {
+        flag_continue <- FALSE
+      }
+      i <- i - 1
+    }
+    return(
+      p_list_sorted[i]
+    )
+  }
+
+#'@export
+compute_fdr <-
+  function(
+    p_list_1,
+    p_list_0,
+    p_value
+  ) {
+      p_list_rejected_1 <-
+        p_list_1[
+          p_list_1 <= p_value
+        ]
+
+      p_list_rejected_0 <-
+        p_list_0[
+          p_list_0 <= p_value
+        ]
+
+      fdr <-
+        length(p_list_rejected_0) / (
+          length(p_list_rejected_0) +
+          length(p_list_rejected_1)
+        )
+
+      return(fdr)
+  }
+
+#'@export
+compute_p_value_benjamini_yekutieli <-
+  function(
+    p_list_1,
+    p_list_0,
+    alpha
+  ) {
+    p_list_sorted <-
+      c(
+        p_list_1,
+        p_list_0
+      ) %>%
+      sort()
+    i <- length(p_list_sorted)
+    flag_continue <- TRUE
+    while (
+      flag_continue & i > 1
+    ) {
+      test <-
+        (
+          p_list_sorted[i] <= (i / M) * alpha / sum(1 / seq_len(M))
+        )
+      if (
+        test
+      ) {
+        flag_continue <- FALSE
+      }
+      i <- i - 1
+    }
+    return(
+      p_list_sorted[i]
+    )
+  }
